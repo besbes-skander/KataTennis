08cc388c5748be1711f122a896ab75a623030b51
Skander <besbes.skander@gmail.com>
2021-05-31 11:09:13 +0200

    refactor

--- src/app/models/game.spec.ts
+++ src/app/models/game.spec.ts
@@ -1,17 +1,22 @@
 import { Game } from './game';
 import {Player} from "./player";
 
-describe('Game', () => {
-  it('should create an instance', () => {
-    const player1 = new Player('Player 1');
-    const player2 = new Player('Player 2');
 
+
+describe('Game', () => {
+  let player1: Player;
+  let player2: Player;
+
+  beforeEach(() => {
+    player1 = new Player('Player 1');
+    player2 = new Player('Player 2');
+  });
+
+  it('should create an instance', () => {
     expect(new Game(player1, player2)).toBeTruthy();
   });
 
   it('should create a game with two players', () => {
-    let player1 = new Player('Player 1');
-    let player2 = new Player('Player 2');
     let game = new Game(player1, player2);
 
     expect(game.player1 instanceof Player).toBeTruthy();
@@ -19,8 +24,6 @@ describe('Game', () => {
   });
 
   it('should return score to 0 when init game', () => {
-    let player1 = new Player('Player 1');
-    let player2 = new Player('Player 2');
     let game = new Game(player1, player2);
 
     expect(game.score()).toEqual('0 - 0');
--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,14 +1,15 @@
 export class Player {
+  private _name;
+
+  constructor(playerName: string) {
+    this._name = playerName;
+  }
+
   get name() {
     return this._name;
   }
 
   set name(value) {
     this._name = value;
-  }
-  private _name;
-
-  constructor(playerName: string) {
-    this._name = playerName;
   }
 }
